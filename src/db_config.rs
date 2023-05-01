205ad948b5b1e1b5c770d4487069b63373caeded
Muhammad Abdur Rafae <114411197+Abdur1Rafae@users.noreply.github.com>
2023-05-01 16:15:53 +0500

    API call to let user record and save their voice tweet completed. voice is stored in a folder named after their objectid, and the voicenote file is named after the mongodb voice_id it is stored as. the voice_id is also stored in the user voiceNotes array.

--- src/db_config.rs
+++ src/db_config.rs
@@ -29,24 +29,25 @@ pub struct Users {
 }
 
 impl Users {
-    pub async fn insert_one(&self, collection: Collection<Users>) -> ObjectId {
+    pub async fn insert_one(&self, collection: Collection<Users>) {
         let new_user = self.clone();
-        let new_user_id = ObjectId::new();
         collection.insert_one(new_user, None).await.unwrap();
-        new_user_id
     }
 }
 
+#[derive(Serialize, Deserialize, Debug)]
 pub enum ReactionType{
     SpeakUp,
     ShutUp,
 }
 
+#[derive(Serialize, Deserialize, Debug)]
 pub struct Reaction{
     user_id:ObjectId,
     reaction: ReactionType
 }
 
+#[derive(Serialize, Deserialize, Debug)]
 pub struct VoiceNote{
     v_id:bson::oid::ObjectId,
     user_id:ObjectId,
@@ -55,17 +56,26 @@ pub struct VoiceNote{
     reactions:Vec<Reaction>,
 }
 
-pub async fn connect_to_mongodb() -> (Collection<Users>, Database, Client) {
+impl VoiceNote{
+    pub async fn insert_one(&self, collection: Collection<VoiceNote>) {
+        let new_vn = self.clone();
+        collection.insert_one(new_vn, None).await.unwrap();
+    }
+}
+
+pub async fn connect_to_mongodb() -> (Collection<Users>, Collection<VoiceNote>, Database, Client) {
     let client = Client::with_uri_str("mongodb+srv://RustUser:RUSTIBA@cluster0.btmwmdh.mongodb.net/test").await.unwrap();
     let db = client.database("Cluster0");
     let collection = db.collection::<Users>("users");
-    println!("reached");
-    (collection , db , client)
+    let vcollection: Collection<VoiceNote>= db.collection::<VoiceNote>("Voice Notes");
+    println!("Connected to MongoDB");
+    (collection, vcollection, db , client)
 }
 
 async fn create_user(user_collection: Collection<Users>, username: String, password: String, name: String) -> ObjectId {
+    let mut user_id = ObjectId::new();
     let new_user = Users {
-        _id: ObjectId::new(),
+        _id: user_id,
         username: username,
         password: password,
         name: name,
@@ -74,8 +84,22 @@ async fn create_user(user_collection: Collection<Users>, username: String, passw
         following: Vec::new(),
         voice_notes: Vec::new(),
     };
-    let new_user_id = new_user.insert_one(user_collection.clone()).await;
-    new_user_id
+    new_user.insert_one(user_collection.clone()).await;
+    user_id
+}
+
+pub async fn create_post(voice_collection: Collection<VoiceNote>, user_collection: Collection<Users>, user_id: ObjectId) -> ObjectId {
+    let mut voice_id = ObjectId::new();
+    let new_voice_note = VoiceNote {
+        v_id: voice_id,
+        user_id: user_id,
+        is_post: true,
+        replies: Vec::new(),
+        reactions: Vec::new(),
+    };
+    new_voice_note.insert_one(voice_collection.clone()).await;
+    save_voice_note(user_collection, user_id, voice_id).await;
+    voice_id
 }
 
 async fn get_user_by_username(collection: Collection<Users>, username: String, password: String) -> Option<Users> {
@@ -139,96 +163,112 @@ pub async fn login(user_collection: Collection<Users>) -> Option<Users> {
     get_user_by_username(user_collection, username, password).await
 }
 
-pub async fn save_voice_note(userid: ObjectId) -> PyResult<()>{
+async fn save_voice_note(collection: Collection<Users> ,userid: ObjectId, v_id: ObjectId) {
+    // Create a filter to match the user with the given ID
+    let filter = doc! { "_id": userid };
 
-    Python::with_gil(|py| {
-        let fun: Py<PyAny> = PyModule::from_code(
-            py,
-            "import pymongo\nimport gridfs\n\
-             def save_sound(user_id):
-                client = pymongo.MongoClient(f'mongodb+srv://RustUser:RUSTIBA@cluster0.btmwmdh.mongodb.net/test')
+    // Create an update document to append the voice note ID to the `voice_notes` array
+    let update = doc! { "$push": { "voice_notes": v_id.clone().to_hex()} };
 
-                # Access a database
-                db = client['Cluster0']
-                fs = gridfs.GridFS(db, collection='fs')
-                with open('hello.wav', 'rb') as f:
-                    contents = f.read()
+    // Create an UpdateOptions instance with default options
+    let options = UpdateOptions::builder().build();
+
+    // Call the update_one method on the collection with the filter, update, and options
+    let result = collection.update_one(filter, update, options).await;
+
+    println!("Audio file saved in MongoDB using GridFS!");
+}
+
+// pub async fn save_voice_note(userid: ObjectId) -> PyResult<()>{
+
+//     Python::with_gil(|py| {
+//         let fun: Py<PyAny> = PyModule::from_code(
+//             py,
+//             "import pymongo\nimport gridfs\n\
+//              def save_sound(user_id):
+//                 client = pymongo.MongoClient(f'mongodb+srv://RustUser:RUSTIBA@cluster0.btmwmdh.mongodb.net/test')
+
+//                 # Access a database
+//                 db = client['Cluster0']
+//                 fs = gridfs.GridFS(db, collection='fs')
+//                 with open('hello.wav', 'rb') as f:
+//                     contents = f.read()
             
-                # Create a new GridFS file and write the contents to it
-                grid_in = fs.new_file(filename='new.wav')
-                grid_in.write(contents)
-                grid_in.close()
+//                 # Create a new GridFS file and write the contents to it
+//                 grid_in = fs.new_file(filename='new.wav')
+//                 grid_in.write(contents)
+//                 grid_in.close()
             
-                # Get the _id of the newly uploaded file
-                file_id = grid_in._id
-                users_collection = db['users']
-                users_collection.update_one(
-                {'_id': user_id},
-                {'$push': {'voice_notes': {'id': file_id}}}
-            )
+//                 # Get the _id of the newly uploaded file
+//                 file_id = grid_in._id
+//                 users_collection = db['users']
+//                 users_collection.update_one(
+//                 {'_id': user_id},
+//                 {'$push': {'voice_notes': {'id': file_id}}}
+//             )
             
-                print('Voicenote ID added to user document successfully.')
-        ",        
+//                 print('Voicenote ID added to user document successfully.')
+//         ",        
 
 
-            "",
-            "",
-        )
-        .expect("function should be called")
-        .getattr("save_sound")?
-        .into();
+//             "",
+//             "",
+//         )
+//         .expect("function should be called")
+//         .getattr("save_sound")?
+//         .into();
     
-        // call object without any arguments
-        let args = PyTuple::new(py, &[userid.to_string()]);
+//         // call object without any arguments
+//         let args = PyTuple::new(py, &[userid.to_string()]);
 
-        fun.call1(py , args);
-        Ok(())
-    })
+//         fun.call1(py , args);
+//         Ok(())
+//     })
  
-}
+// }
 
 
-pub async fn play_audio(v_id : &str) -> PyResult<()> {
-    Python::with_gil(|py| {
-        let fun: Py<PyAny> = PyModule::from_code(
-            py,
-            "import pymongo\nimport gridfs\nimport playsound\n\
-            def download_audio_file( file_id):
+// pub async fn play_audio(v_id : &str) -> PyResult<()> {
+//     Python::with_gil(|py| {
+//         let fun: Py<PyAny> = PyModule::from_code(
+//             py,
+//             "import pymongo\nimport gridfs\nimport playsound\n\
+//             def download_audio_file( file_id):
             
-                client = pymongo.MongoClient(f'mongodb+srv://RustUser:RUSTIBA@cluster0.btmwmdh.mongodb.net/test')
+//                 client = pymongo.MongoClient(f'mongodb+srv://RustUser:RUSTIBA@cluster0.btmwmdh.mongodb.net/test')
             
-                # Access a database
-                db = client['Cluster0']
-                fs = gridfs.GridFS(db, collection='fs')
-                # Find the audio file in GridFS
-                grid_out = fs.find_one({'_id': file_id})
+//                 # Access a database
+//                 db = client['Cluster0']
+//                 fs = gridfs.GridFS(db, collection='fs')
+//                 # Find the audio file in GridFS
+//                 grid_out = fs.find_one({'_id': file_id})
             
-                # Read the audio file data into a variable
-                audio_data = grid_out.read()
+//                 # Read the audio file data into a variable
+//                 audio_data = grid_out.read()
             
-                # Write the audio file data to a local file with a .wav extension
-                with open('downloaded.wav', 'wb') as f:
-                    f.write(audio_data)
-                playsound('downloaded.wav')
+//                 # Write the audio file data to a local file with a .wav extension
+//                 with open('downloaded.wav', 'wb') as f:
+//                     f.write(audio_data)
+//                 playsound('downloaded.wav')
         
-        ",        
+//         ",        
 
 
-            "",
-            "",
-        )
-        .expect("function should be called")
-        .getattr("download_audio_file")?
-        .into();
+//             "",
+//             "",
+//         )
+//         .expect("function should be called")
+//         .getattr("download_audio_file")?
+//         .into();
     
-        // call object without any arguments
-        let args = PyTuple::new(py, &[v_id.to_string()]);
+//         // call object without any arguments
+//         let args = PyTuple::new(py, &[v_id.to_string()]);
 
-        fun.call1(py , args);
+//         fun.call1(py , args);
 
-        Ok(())
-    })
+//         Ok(())
+//     })
      
-}
+// }
 
 fn main() {}
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 mod db_config;
 use std::fs;
 
-use db_config::connect_to_mongodb;
+use db_config::{connect_to_mongodb, VoiceNote};
 use record_audio::audio_clip::AudioClip as ac;
 use tokio;
 use std::io;
@@ -10,14 +10,14 @@ use mongodb::{Collection, bson::oid::ObjectId};
 #[tokio::main]
 
 async fn main() {
-    let (user_collection, DB, client) = connect_to_mongodb().await;
+    let (user_collection, voice_note_collection, DB, client) = connect_to_mongodb().await;
 
     let mut input = String::new();
 
     let mut user_id=ObjectId::new();
     let mut authenticated = false;
 
-    while(!authenticated){
+    while !authenticated{
         println!("Do you want to login or signup? (l/s): ");
         match io::stdin().read_line(&mut input) {
             Ok(_) => {
@@ -46,31 +46,35 @@ async fn main() {
 
     // Call the create_user function to create a new user
     println!("User created with ID: {:?}", user_id);
+    let folder_name = format!("{}", user_id);
+    fs::create_dir_all(&folder_name).unwrap(); // use create_dir_all to create the folder and its parent folders if they don't exist
 
+    println!("Would you like to tweet something? (y, n)");
 
-    let file_name = "hello.wav";
-    match ac::record(None) {
-        Ok(clip) => {
-            println!("Successfully recorded!");
-            match clip.export(format!("{}" , file_name).as_str()) {
-                Ok(_) => {
-                    println!("Successfully saved as hello.wav");
-                    
-                    //to play immediately, after ctrl-c
-                    //clip.play().unwrap();
+    input = String::new();
+
+    match io::stdin().read_line(&mut input) {
+        Ok(_) => {
+            if input.trim() == "y" {
+                let mut file_name= ObjectId::new();
+                file_name= db_config::create_post(voice_note_collection.clone(), user_collection.clone(), user_id).await;
+                let directory = format!("{}/{}.wav", folder_name, file_name.to_hex());
+                match ac::record(None) {
+                    Ok(clip) => {
+                        println!("Successfully recorded!");
+                        match clip.export(format!("{}" , directory).as_str()) {
+                            Ok(_) => {
+                                println!("Successfully saved!");
+                            }
+                            Err(err) => println!("Error {}", err),
+                        }
+                    }
+                    Err(err) => println!("Error {}", err),
                 }
-                Err(err) => println!("Error {}", err),
             }
         }
-        Err(err) => println!("Error {}", err),
-    }
-
-  let v_id =  db_config::save_voice_note(user_id).await;
-  
-  let path = "hello.wav";
-    match fs::remove_file(path) {
-        Ok(()) => println!("File deleted successfully!"),
-        Err(e) => println!("Error deleting file: {}", e),
-    }  
-  db_config::play_audio("644e4397f424bc54fca3b912").await;
+        Err(e) => {
+            println!("Error: {}", e);
+        }
+    };
 }
